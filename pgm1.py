9aaabc6353180e3dc35f663d75647a9d685b5bcc
MeghaCF <megha.2012mf@gmail.com>
2020-04-25 07:46:31 +0530

    First time pushing

---
+++ pgm1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  7 12:19:14 2020
+
+@author: Dell
+"""
+
+a=56
+b=23
+print (a)
+print (b)
+c=a+b
+print(c)
+
+print("hello everyone")
+a=10
+b=20
+print(a+b)
+
+
+       
